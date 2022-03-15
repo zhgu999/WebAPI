ca42f40d5ebb981c28392ff7dd01c6346f4d7fcc
Gauss3Maxwell <Gauss3Maxwell@outlook.com>
2022-03-15 16:07:14 +0800

    getbalance

--- btca.js
+++ btca.js
@@ -10,9 +10,9 @@ const url = 'http://127.0.0.1:9904';
 const conn = mysql.createConnection({
   host: '127.0.0.1',
   port: '3306',
-  user: 'btca',
+  user: 'bbc',
   password: '1234qwer',
-  database: 'btca'
+  database: 'bbc'
 });
 
 conn.connect();
@@ -139,6 +139,136 @@ app.get('/banners', function(req, res, next) {
   res.json(json);
 });
 
+
+//http://127.0.0.1:7711/unspent?address=1yq024eeg375yvd3kc45swqpvfz0wcrsbpz2k9escysvq68dhy9vtqe58&symbol=BBC
+app.get('/unspent', function(req, res, next) {
+  console.log('unspent');
+  if (req.query.symbol != 'BBC') {
+    res.json([]);
+    return;
+  }
+  request({
+    url: url,
+    method: 'POST',
+    json: true,
+    body: {'id':3,'method':'listunspent','jsonrpc':'2.0','params':{'address':req.query.address}}
+  },function (error, response, body) {
+    if (body.error) {
+      res.json(body.error);
+    } else {
+      if (body.result.addresses.length == 1) {
+        res.json(body.result.addresses[0].unspents);
+      } else {
+        res.json([]);
+      }
+    }
+  });
+});
+
+app.get('/transaction', function(req, res, next) {
+  //http://127.0.0.1:7711/transaction?address=1yq024eeg375yvd3kc45swqpvfz0wcrsbpz2k9escysvq68dhy9vtqe58&symbol=BBC
+  console.log('transaction');
+  if (req.query.symbol != 'BBC') {
+    res.json({'unconfirmed':'0','balance':0});
+    return;
+  }
+  /*
+  {
+    'hash':
+        '61c0565fc9007d4447261aaf8351eecce9ad676df2db15450d31e23c6fa983ab',
+    'fromAddress': address,
+    'toAddress':
+        '1549pyzf8dhx7r4x40k5j80f12btkpqfprjp134bcgcrjn963nzsx57xb',
+    'timestamp': 1639994975,
+    'confirmed': 180,
+    'txFee': 0.0137,
+    'amount': 10.01
+  }
+  */
+  let sql = 'select txid as `hash`,form as fromAddress,`to` as toAddress,transtime as `timestamp`,1 as confirmed,free as txFee, amount from Tx where `to` = ? or form = ? order by id desc limit 10;';
+  let params = [req.query.address,req.query.address];
+  conn.query(sql,params,function(err,result) {
+    if(err){
+      res.json({'error':err});
+      return;
+    }
+    let dataString =JSON.stringify(result);
+    res.send(JSON.parse(dataString));
+  });
+});
+
+app.get('/balance', function(req, res, next) {
+  //http://127.0.0.1:7711/balance?address=1yq024eeg375yvd3kc45swqpvfz0wcrsbpz2k9escysvq68dhy9vtqe58&symbol=BBC
+  //77f2b1217377f62cbb34c5b72b63c6c17fdb5e9e0b6173b4edcb19d03922c0f5
+  //res.json({'address': req.query.address,'symbol': req.query.symbol});
+  console.log('balance');
+  if (req.query.symbol != 'BBC') {
+    res.json({'unconfirmed':'0','balance':0});
+    return;
+  }
+  let sql = 'select * from addr where bbc_addr = ?';
+  let params = [req.query.address];
+  conn.query(sql,params,function(err,result){
+    if (err) {
+      res.json({'error':err});
+      return;
+    }
+    if (result.length == 0) {
+       request(
+        {
+          url: url,
+          method: 'POST',
+          json: true,
+          body:{'id':1,'method':'getpubkey','jsonrpc':'2.0','params':{'privkeyaddress': req.query.address}}
+        },
+        function(error, response, body) {
+          if (body.error) {
+            res.json(body.error);
+          } else {
+            request({
+              url: url,
+              method: 'POST',
+              json: true,
+              body:{'id':2,'method':'importpubkey','jsonrpc':'2.0','params':{'pubkey': body.result}}
+            },function(error, response, body) {
+              if (body.error) {
+                res.json(body.error);
+              } else {
+                request({
+                  url: url,
+                  method: 'POST',
+                  json: true,
+                  body: {'id':3,'method':'getbalance','jsonrpc':'2.0','params':{'address':body.result}}
+                },function (error, response, body) {
+                  if (body.error) {
+                    res.json(body.error);
+                  } else {
+                    sql = 'insert into addr(bbc_addr)values(?)';
+                    conn.query(sql,[req.query.address],function(err,result){
+                      res.json({'unconfirmed':body.result[0].unconfirmed,'balance':body.result[0].avail});
+                    });
+                  }
+                });
+              }
+            });
+          }
+        });
+    } else {
+      request({
+        url: url,
+        method: 'POST',
+        json: true,
+        body: {'id':1,'method':'getbalance','jsonrpc':'2.0','params':{'address':req.query.address}}
+      },function (error, response, body) {
+        if (body.error) {
+          res.json(body.error);
+        } else {
+          res.json({'unconfirmed':body.result[0].unconfirmed,'balance':body.result[0].avail});
+        }
+      })
+    }
+  });
+});
 
 let server = app.listen(7711, function() {
   let host = server.address().address;
